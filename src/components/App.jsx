cf4e36ac2d3e9d6ec259713316919950afc400e6
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-06-12 14:04:22 +0100

    first-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      Hambee Studio
+      <h1>Hambee Studio</h1>
     </div>
   );
 };
